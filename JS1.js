be242477081c612323c5bd7de48f04ef5366861e
Yash Srivastava <120123563+yashsrivasta7a@users.noreply.github.com>
2024-07-13 12:26:22 +0530

    JS1

---
+++ JS1.js
@@ -0,0 +1,45 @@
+// Task 1
+var Number = 45;
+console.log(Number);
+
+
+// Task 2
+let Name = "Sharma";
+console.log(Name);
+
+// Task 3
+const Flag = false;
+console.log(Flag);
+
+// Task 4
+const number = 18;
+const name = "Kohli";
+const goat = true ;
+const info ={
+    "wife" : "anushka sharma",
+    "child": "yes"
+} 
+const infoArray = [
+    "wife" , "anushka sharma",
+    "child", "yes"
+]
+
+
+console.log(typeof number); //number
+console.log(typeof name ); //string
+console.log(typeof goat ); //boolean
+console.log(typeof info ); //object
+console.log(typeof infoArray ); // object: Arrays are objects in js
+
+
+// Task 5
+let a = "Yash"
+console.log(a);
+a = "YS7"
+console.log(a);
+
+// Task 6
+const b = "Yash"
+console.log(b); //error:beacuse of const keyword become constant 
+b = "YS7"
+console.log(b);
