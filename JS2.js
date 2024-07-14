e33010a0e338516c75c7c23044ab37bc5289a52e
Yash Srivastava <120123563+yashsrivasta7a@users.noreply.github.com>
2024-07-14 10:53:08 +0530

    JS2

---
+++ JS2.js
@@ -0,0 +1,45 @@
+// Task 1
+const a = 1
+const b = 2
+console.log(a+b);
+
+// Task 2
+console.log(a-b);
+
+// Task 3
+console.log(a*b);
+
+// Task 4
+console.log(b/a);
+
+// Task 5
+console.log(a%b);
+
+// Task 6
+let c=10
+console.log(c+=a);
+
+//task 7
+console.log(c-=a);
+
+// task 8,9,10
+console.log(a>b);
+console.log(a<b);
+console.log(a==b);
+console.log(a===b);
+
+//task 11,12,13
+if(a<c && a==b){
+    console.log("Statement is True");
+}
+else(a<c || a==b)
+    console.log("Statement is False");
+
+    if(!(a<c && a==b)){
+        console.log("! op is used ");
+    }
+
+
+let d = 20;
+let ans = 10 < d ? "positive":"negative";
+console.log(ans)
