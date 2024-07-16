959e2c6489b2437484912a7a5a408a27a55bca56
Yash Srivastava <120123563+yashsrivasta7a@users.noreply.github.com>
2024-07-16 16:46:44 +0530

    js4

---
+++ JS4.js
@@ -0,0 +1,72 @@
+//TASK1
+for ( let i=0;i<10;i++){
+    console.log(i+1);
+}
+
+console.log("_________________");
+
+//TASK2
+for ( let i=0;i<10;i++){
+    console.log((i+1)*5);
+}
+
+console.log("_________________");
+
+//TASK3
+let a=0, i=0;
+while (i<=10) {
+    console.log( a+=i);
+    i++;
+    
+}
+
+
+console.log("_________________");
+
+
+//TASK 4
+for(let i=10;i>=1;i--){
+    console.log(i);
+}
+
+console.log("_________________");
+
+//TASK 5
+let b=1;
+do {
+    console.log(b);
+    b++;
+} while (b<=5);
+
+
+console.log("_________________");
+
+//TASK 6
+
+let c = 5;
+let p = 1;
+let j = c;
+
+do {
+    p *= j;  
+    j--;     
+} while (j > 0);
+
+console.log(p);
+
+
+console.log("_________________");
+
+// TASK 7
+
+for (let i = 1; i <= 5; i++) {
+    let pattern = "";
+    for (let j = 0; j < i; j++) {
+        pattern = pattern + "*";
+    }
+    console.log(pattern);
+}
+
+
+// TASK 8
+
